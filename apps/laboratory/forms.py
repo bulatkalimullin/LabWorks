70f628982bf5a0e57502027c887881b2ec62cdf8
nollieundergrob <92156277+nollieundergrob@users.noreply.github.com>
2025-10-08 22:26:55 +0300

    asf

--- apps/laboratory/forms.py
+++ apps/laboratory/forms.py
@@ -5,7 +5,7 @@ from .models import CustomUser, Submission, Course, StudentGroup, Assignment
 
 import re
 
-class CustomSelectMultiple(forms.SelectMultiple):
+class CustomSelectMultiple(forms.Select):
     def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
         option = super().create_option(name, value, label, selected, index, subindex, attrs)
         if value == '':
